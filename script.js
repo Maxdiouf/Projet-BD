d734929f58b3823f1b268dff4d1ee3fd964c2de1
Maxdiouf <dioufmaguette17@gmail.com>
2022-11-25 16:46:00 +0100

    <commit_message>

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Fonction animation onomatopé
 console.log('go')
 function clignotement(){ 
     if (document.getElementById("onoo2").style.display=="block"){
@@ -10,3 +11,15 @@ function clignotement(){
 // mise en place de l appel régulier de la fonction toutes les 1.2 secondes 
 setInterval("clignotement()", 1000);
 //dans la balise body on appelle le fichier script1.js pour lui assigner : <onload="script1.js">car ma page javascript est nommé test
+
+function changementImage(sourceImage) {
+    document.getElementById('livre1').src = sourceImage
+    //document.getElementByid('livre1').style.height= 300 +"px";
+
+}
+function changementImagesss(sourceImage) {
+    document.getElementById('livre1').src = sourceImage
+    //document.getElementByid('livre1').style.height= 100 +"px";
+    console.log('test')
+    
+}
