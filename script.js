b031c0795719f8cff77e5535e69e3c9c78fa0200
Gajan Baskaran <gajanbaskaran@gmail.com>
2023-01-15 18:58:44 +0100

    UPDATE
    update

--- script.js
+++ script.js
@@ -28,3 +28,22 @@ document.querySelectorAll('.thumbnails img').forEach(el => {
 slideGallery.addEventListener('scroll', e => scrollThumb());
 
 scrollThumb();
+
+
+/* MENU OUVRANT FONCTION */
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
