33b039a05ef16c5d97e2acaf4210c731627cb704
Maxdiouf <dioufmaguette17@gmail.com>
2023-01-21 02:44:30 +0100

    commit_message

---
+++ FrancoBelge/neuvieme_art/neuvieme_art.js
@@ -0,0 +1,17 @@
+console.log("ok")
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+function openNav() {
+  sidenav.classList.add("active");
+  console.log("ok")
+}
+
+function closeNav() {
+  sidenav.classList.remove("active");
+  console.log("ok")
+}
